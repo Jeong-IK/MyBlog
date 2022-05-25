9ff3ef4807024a9bf83c39031166dd61918a5670
Im Free <67920695+Jeong-IK@users.noreply.github.com>
2022-05-25 16:09:39 +0900

    Create Header, mainBody Components

---
+++ myblog/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./Header/header";
+import { mainBody } from "./Body/mainBody";
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <mainBody/>
+    </div>
+  );
+}
+
+export default App;
