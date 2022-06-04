f46d710a7d24d4fcf44d7fec1959272f15c85769
JKMA-3 <jjy20315@gmail.com>
2022-06-04 22:56:29 +0900

    [+Code] Background IMG

--- myblog/src/Components/App.js
+++ myblog/src/Components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Header } from "./Header/Header";
 import { MainBody } from "./Body/MainBody";
-
+import { BgImg } from "./BgIMG/BgIMG";
 function App() {
   return (
     <div>
+      <BgImg />
       <Header />
       <MainBody />
     </div>
---
+++ myblog/src/Components/BgIMG/BgIMG_style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Background_Img = styled.div`
+  background-image: url("./img/white.jpg");
+  background-size: cover;
+`;
