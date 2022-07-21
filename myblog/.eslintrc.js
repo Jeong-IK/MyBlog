011e805c62a460867468e44e1eecabf150405991
Jeong-IK <jjy20315@gmail.com>
2022-07-21 17:57:39 +0900

    Change Setting

--- myblog/.eslintrc.js
+++ myblog/.eslintrc.js
@@ -1,35 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['airbnb-base', 'plugin:react/recommended', 'plugin:import/recommended', 'plugin:import/typescript'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    //
-    'import/prefer-default-export': 'off',
-    // 운영체제 마다 개행 차이로 생기는 오류
-    'linebreak-style': 0,
-    //
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        tsx: 'never',
-        ts: 'never',
-        js: 'never',
-        jsx: 'never',
-      },
+    "extends": "plugin:react/recommended",
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
     ],
-  },
-};
+    "rules": {
+    }
+}
