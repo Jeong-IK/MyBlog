cd3796de483e84d22879d3e1d87372fb1c05e8b7
Jeong-IK <jjy20315@gmail.com>
2022-07-27 15:52:18 +0900

    Card 스타일 수정

--- myblog/src/Components/Body/Contents/Project/RepositoryCard/RepositoryCard_style.ts
+++ myblog/src/Components/Body/Contents/Project/RepositoryCard/RepositoryCard_style.ts
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-height: 150px;
-width: 120px;
-border-radius: 15px;
-display: inline-block;
+height: 3.5rem;
+width: 15rem;
+border-radius: 5px;
+display: flex;
 margin: 15px;
 position: relative;
 box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 overflow: hidden;
+justify-content: center;
+align-items: center;
+`;
+
+export const ReposeName = styled.h1`
+padding: 0.1rem;
+font-size:1rem;
+
 `;
--- myblog/src/Components/Body/Contents/Project/RepositoryCard/index.tsx
+++ myblog/src/Components/Body/Contents/Project/RepositoryCard/index.tsx
@@ -1,7 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import { repositoryData } from '../Project_type';
-import { Card } from './RepositoryCard_style';
+import { Card, ReposeName } from './RepositoryCard_style';
 
 export const RepositoryCard: FunctionComponent<repositoryData> = props => {
-  return <Card>{props.name}</Card>;
+  return (
+  <Card>
+    <ReposeName>{props.name}</ReposeName>
+  </Card>
+  );
 };
