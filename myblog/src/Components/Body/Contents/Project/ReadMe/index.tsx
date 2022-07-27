d11ce3f4ef6975ab59a2984f2d354e4675c904c3
Jeong-IK <jjy20315@gmail.com>
2022-07-27 15:54:09 +0900

    ReadMe디렉토리 추가

---
+++ myblog/src/Components/Body/Contents/Project/ReadMe/index.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent } from "react";
+import { ReviewData } from './ReadMe_style';
+
+export const ReadMe:FunctionComponent = () =>{
+    return (<ReviewData>ReadMe</ReviewData>)
+}
