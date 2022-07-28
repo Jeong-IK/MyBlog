29544595a1286e0f548aa9d3b1533aeff7bd548c
Jeong-IK <jjy20315@gmail.com>
2022-07-28 17:26:18 +0900

    API

--- myblog/src/Components/Body/Contents/Project/ReadMe/index.tsx
+++ myblog/src/Components/Body/Contents/Project/ReadMe/index.tsx
@@ -2,5 +2,6 @@ import React, { FunctionComponent } from "react";
 import { ReviewData } from './ReadMe_style';
 
 export const ReadMe:FunctionComponent = () =>{
+    /*https://api.github.com/repos/OWNER/REPO/contents/PATH */
     return (<ReviewData>ReadMe</ReviewData>)
 }
