bc54d61117df2b7706ef6205aeb0b20437d62e7e
Jeong-IK <jjy20315@gmail.com>
2022-08-03 17:31:46 +0900

    Sns 스타일 조정

--- myblog/src/Components/Body/Contents/SNS/Sns.tsx
+++ myblog/src/Components/Body/Contents/SNS/Sns.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Button } from './Button';
+import { SnsList } from './Sns_style';
 
 export const Sns = () => {
     return (
-                <>
+                <SnsList>
                     <Button name='velog' url='https://velog.io/@han1368'></Button>
                     <Button name='Github' url='https://github.com/Jeong-IK'></Button>
-                </>
+                </SnsList>
             )};
---
+++ myblog/src/Components/Body/Contents/SNS/Sns_style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SnsList = styled.div`
+display flex;
+justify-content: center;
+align-items: center;
+`;
